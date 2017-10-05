f0712bcff6e6d614fa120f316966f6160d10000f
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-05 15:06:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,7 @@ end
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(index)
-      move(index, current_player(board))
+      move(index, current_player)
       display_board
     else
       turn
