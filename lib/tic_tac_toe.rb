260e50ef76a007785136f3f84f1458c50c8a0fec
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-05 15:01:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ def board
 @board
 end
 
-  def display_board(board)
+  def display_board
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
